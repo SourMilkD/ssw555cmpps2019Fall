658d643580d6380d1d76de404196dbb201be9a6f
Paul Szenher <pszenher@stevens.edu>
2019-09-17 03:29:13 -0400

    Return level number token as int instead of string in line parser

--- parser.py
+++ parser.py
@@ -25,15 +25,15 @@ def parse_line(raw_line):
     if token[0] in valid_tags.keys():
         # If special tags are in 3rd position, return valid
         if len(token) > 2 and token[0] == "0" and token[2] in ("INDI", "FAM"):
-            return token[0], token[2], True, token[1]
+            return int(token[0]), token[2], True, token[1]
         # If standard tags match their designated level, return valid
         if token[1] in valid_tags.get(token[0]):
-            return token[0], token[1], True, " ".join(token[2:])
+            return int(token[0]), token[1], True, " ".join(token[2:])
         # Else, return invalid
-        return token[0], token[1], False, " ".join(token[2:])
+        return int(token[0]), token[1], False, " ".join(token[2:])
     else:
         # If level number invalid, return invalid
-        return token[0], token[1], False, " ".join(token[2:])
+        return int(token[0]), token[1], False, " ".join(token[2:])
 
 
 def main():
