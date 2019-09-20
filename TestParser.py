1a64da30cab10dec41ccbe2c8d370514cc49ae4f
Mike Ryan <mryan6@stevens.edu>
2019-09-20 12:50:34 -0400

    created unittest file

---
+++ TestParser.py
@@ -0,0 +1,14 @@
+"""
+  SSW/CS 555
+  Michael Ryan, Pat Song, Soumik Deb, Connor Santiago, Paul Szenher
+  This file will contain all the test cases for all of our user stories
+"""
+
+import unittest
+
+
+class TestParser (unittest.TestCase) :
+
+    #example unittest
+    def testUS00(self):
+        self.assertEqual(1,1,'1 Should be Equal to 1')
